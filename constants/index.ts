bdc3c705be00b3492f9c00084830cdd25dfef8e4
Eduard Aksamitov <e@edaks.co>
2021-11-04 23:42:49 +0300

    wip: init

---
+++ constants/index.ts
@@ -0,0 +1,39 @@
+export const USER_STATUS_ACTIVE = 100
+export const USER_STATUS_REMOVED = 200
+export const USER_STATUS_BANNED = 200
+
+export const PROJECT_STATUS_ACTIVE = 100
+export const PROJECT_STATUS_REMOVED = 200
+
+export const PROJECT_MEMBER_ROLE_CREATOR = 1000
+export const PROJECT_MEMBER_ROLE_MANAGER = 100
+export const PROJECT_MEMBER_ROLE_MEMEBER = 200
+export const PROJECT_MEMBER_ROLE_VIEWER = 300
+
+export const PAGE_STATUS_ACTIVE = 100
+export const PAGE_STATUS_DISCONNECTED = 300
+
+export const POST_STATUS_CREATED = 100
+export const POST_STATUS_REMOVED = 200
+export const POST_STATUS_SCHEDULED = 300
+export const POST_STATUS_ERRORED = 400
+export const POST_STATUS_POSTED = 500
+
+export const POST_TYPE_POST = 100
+export const POST_TYPE_DRAFT = 200
+export const POST_TYPE_IMPORTED = 300
+export const POST_TYPE_SUGGESTED = 400
+
+export const NETWORKS_ORDER = [
+	'facebook',
+	'instagram',
+	'twitter',
+	'vk'
+]
+
+export const NETWORKS_ABBR = {
+	facebook: 'fb',
+	instagram: 'ig',
+	twitter: 'tw',
+	vk: 'vk'
+}
